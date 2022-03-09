425701727f9eb349a615b629d8a645db465e57ca
陈泉 <authemail@qq.com>
2022-03-09 18:47:11 +0800

    feat: add 404 page router

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import Features from './pages/Features'
+import NotFound from './pages/NotFound'
 import Welcome from './pages/Welcome'
 
 function AppRouter() {
     return (
         <Routes>
+            {/* 404 Page */}
+            <Route path='*' element={<NotFound />} />
+            {/* Other Pages */}
             <Route path={'/'} element={<Welcome />} />
             <Route path={'/features'} element={<Features />} />
         </Routes>
